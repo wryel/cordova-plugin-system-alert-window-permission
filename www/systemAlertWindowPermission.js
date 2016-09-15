f3fcd08638563679ac1acba147f3a2902118f346
Wryel Covo <ryryel@gmail.com>
2016-09-15 15:17:48 -0300

    callback

--- www/systemAlertWindowPermission.js
+++ www/systemAlertWindowPermission.js
@@ -1,10 +1,13 @@
 /*global cordova, module*/
 
+var systemAlertWindowPermissionName = "SystemAlertWindowPermission";
+
 module.exports = {
+    SYSTEM_ALERT_WINDOW: "android.permission.SYSTEM_ALERT_WINDOW",
     hasPermission: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "SystemAlertWindowPermission", "hasPermission", []);
+        cordova.exec(successCallback, errorCallback, systemAlertWindowPermissionName, "hasPermission", []);
     },
     requestPermission: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "SystemAlertWindowPermission", "requestPermission", []);
+        cordova.exec(successCallback, errorCallback, systemAlertWindowPermissionName, "requestPermission", []);
     }
 };
